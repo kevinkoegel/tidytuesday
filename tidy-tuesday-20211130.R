1c65b0028ddced4e2704d74660676eab98bceea3
Kevin <69598159+kevinkoegel@users.noreply.github.com>
2021-12-01 22:02:32 -0700

    Update 20211130 code and plot

--- tidy-tuesday-20211130.R
+++ tidy-tuesday-20211130.R
@@ -1,6 +1,6 @@
 #load packages
 library(tidyverse)
-library(tidyr)
+library(ggrepel)
 
 #import data
 matches <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-11-30/matches.csv')
@@ -25,12 +25,16 @@ matchesbyteam <- matches %>%
     TRUE ~ 'loss'
   ))
 
-mostfrequent <- matchesbyteam %>%
+counts <- matchesbyteam %>%
   group_by(team) %>%
-  count() %>%
-  filter(n > 50) 
+  count()
 
-win_perc <- mostfrequent %>%
+home_counts <- matchesbyteam %>%
+  filter(home_away == 'home') %>%
+  group_by(team) %>%
+  count()
+
+win_perc <- counts %>%
   left_join(matchesbyteam, by = 'team') %>%
   rename('total_matches' = n) %>%
   group_by(team, total_matches, win_or_loss) %>%
@@ -42,36 +46,61 @@ win_perc <- mostfrequent %>%
   ungroup() %>%
   select(team, overall_win_percentage)
 
-summary <- mostfrequent %>%
+home_win_perc <- home_counts %>%
   left_join(matchesbyteam, by = 'team') %>%
-  left_join(win_perc, by = 'team') %>%
   rename('total_matches' = n) %>%
-  group_by(team, total_matches, overall_win_percentage, home_away, win_or_loss) %>%
+  filter(home_away == 'home') %>%
+  group_by(team, total_matches, win_or_loss) %>%
+  count(win_or_loss) %>%
+  pivot_wider(names_from = win_or_loss, values_from = n) %>%
+  replace(., is.na(.), 0) %>%
+  mutate(home_win_percentage = win/total_matches) %>%
+  ungroup() %>%
+  select(team, home_win_percentage)
+
+summary <- counts %>%
+  left_join(matchesbyteam, by = 'team') %>%
+  left_join(home_win_perc, by = 'team') %>%
+  rename('total_matches' = n) %>%
+  group_by(team, total_matches, home_win_percentage, venue, home_away, win_or_loss) %>%
   count(win_or_loss) %>%
   pivot_wider(names_from = win_or_loss, values_from = n) %>%
   replace(., is.na(.), 0) %>%
   mutate(number_of_matches = win + loss,
-         percent_home_away = scales::percent(number_of_matches/total_matches, accuracy=1),
-         win_percentage = scales::percent(win/number_of_matches, accuracy=1),
-         loss = loss*-1) %>%
-  pivot_longer(cols = c(win, loss), names_to = 'win_or_loss') %>%
-  unite('team_home_away', c(team,home_away), remove = FALSE)
+         percent_home_away = number_of_matches/total_matches,
+         win_percentage = win/number_of_matches)
 
-home_away_win_perc <- summary_all %>%
-  ungroup() %>%
-  select(team, home_away, win_percentage)
-
-home_only <- summary %>%
-  filter(home_away == 'home')
-
-win_only <- summary %>%
-  filter(win_or_loss == 'win')
+venues <- summary %>%
+  filter(venue == 'R Premadasa Stadium, Colombo' |
+           venue == 'Arnos Vale Ground, Kingstown, St Vincent',
+         home_away == 'home')
 
 #plot
 summary %>%
-  ggplot(aes(x = value,
-             y = reorder(team_home_away, overall_win_percentage))) +
-  geom_col(fill = 'lightgray') +
-  geom_vline(xintercept = 0, color = 'gray50') +
-  geom_text(data = win_only, aes(label = win_percentage)) 
-  
+  filter(total_matches > 50,
+         home_away == 'home') %>%
+  ggplot(aes(x = win_percentage,
+             y = reorder(team, home_win_percentage))) +
+  geom_point(aes(size = win, color = team), alpha = 3/4, position = 'jitter') +
+  geom_text_repel(data = venues, aes(label = venue), 
+                  size = 2.5, min.segment.length = 0, nudge_x = -.2) +
+  theme(axis.ticks = element_blank(),
+        axis.text.x = element_text(size = 9),
+        axis.title.x = element_text(size = 9),
+        axis.title.y = element_blank(),
+        panel.grid.major.x = element_line(color = 'gray93'),
+        panel.background = element_blank(),
+        plot.title = element_text(face = 'bold', size = 12),
+        plot.title.position = 'plot',
+        plot.subtitle = element_text(size = 10),
+        legend.title = element_text(size = 9),
+        plot.caption = element_text(size = 8, hjust = 1.15)) +
+  coord_cartesian(clip = 'off') +
+  scale_x_continuous(labels = scales::percent_format(scale = 100)) +
+  guides(color = 'none') +
+  labs(title = 'Which nations had the best "home oval advantage" in ODI cricket, 1996-2005?',
+       subtitle = 'Sri Lanka fared the best at home (among nations competing in at least 50 matches) with an 81% win \npercentage in 81 ODI matches as the home side, including 33 wins at R Premadasa Stadium, Colombo. \nWest Indies had a 92% (!) winning percentage at Arnos Vale Ground, Kingstown, St Vincent. \nAustralia (69%) and South Africa (68%) had the highest overall winning percentages over the period.',
+       x = 'Win percentage per home venue',
+       size = 'Number \nof wins',
+       caption = 'SOURCE: ESPN Cricinfo') +
+  scale_color_manual(values = c('yellow1','seagreen','gray','dodgerblue1','darkgreen','black','darkgreen','darkgreen','navyblue','maroon','red2'))
